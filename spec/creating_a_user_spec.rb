2c35a12f78f4045715e4e88cf97347e75c29062a
Clint <clintp1@gmail.com>
2015-06-26 07:42:54 +0100

    updated creating a user test

--- spec/creating_a_user_spec.rb
+++ spec/creating_a_user_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe 'adding a voucher' do
-	it 'should be able to add a voucher code to a order of $10'do
-	    visit '/users/new'
-	    expect(page).to have_content("hello world")
+describe 'creating a user' do
+	it 'should be able to create a user' do
+	    visit '/users/sign_up'
+	    expect(page).to have_content("Sign up")
 	end
 end
